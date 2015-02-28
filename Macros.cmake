547f4e45807870d50e2e420b670b3f4ead1a6571
Zenith <samuel.ghinet@gmail.com>
2015-02-28 18:16:28 +0200

    Create CMake macro CreateTests and use it
    We use this functionality in XLib as well.

--- Macros.cmake
+++ Macros.cmake
@@ -3,3 +3,16 @@ macro(CreateTestProj name fileName)
     target_link_libraries(${name} gmock gmock_main gtest gtest_main XLib ${ARGN})
     add_test(NAME ${name} COMMAND ${name})
 endmacro(CreateTestProj)
+
+macro(CreateTests prefix addIncludeDirs addLinkTargets)
+    include_directories("${GTEST_INCLUDE_DIR}" "${GMOCK_INCLUDE_DIR}" "${addIncludeDirs}")
+    link_directories("${GTEST_LIB_DIR}" "${GMOCK_LIB_DIR}")
+
+    file(GLOB_RECURSE CPP_FILES "${CMAKE_CURRENT_SOURCE_DIR}" "*.cpp")
+    source_group("Test" FILES ${CPP_FILES})
+
+    foreach(curItem ${CPP_FILES})
+        get_filename_component(displayName "${curItem}" NAME_WE)
+        CreateTestProj("${prefix}${displayName}" ${curItem} ${addLinkTargets})
+    endforeach()
+endmacro(CreateTests)
