bde40dd2b44ebabbacea6706c66dc90ca5a646a8
Samuel Ghinet <samuel.ghinet@gmail.com>
2015-09-26 21:59:55 +0300

    remove obsolete doc comment

--- zSearch/zSearch/zSearch.h
+++ zSearch/zSearch/zSearch.h
@@ -16,14 +16,6 @@
 
 #pragma once
 
-/* The following ifdef block is the standard way of creating macros which make exporting 
-*  from a DLL simpler. All files within this DLL are compiled with the ZCORE_EXPORTS
-*  symbol defined on the command line. This symbol should not be defined on any project
-*  that uses this DLL. This way any other project whose source files include this file see
-*  ZCORE_API functions as being imported from a DLL, whereas this DLL sees symbols
-*  defined with this macro as being exported.
-*/
-
 #if PROJ_LINK_SHARED
 #ifdef ZSEARCH_EXPORTS
 #ifdef WIN32
